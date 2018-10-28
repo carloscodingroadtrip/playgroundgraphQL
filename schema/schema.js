4671955d19173abec38729b8724267a4e2c09b81
Carlos Molina <molina.carlos.dev@gmail.com>
2018-10-28 11:51:11 -0500

    Server and graphiQL has been setup

---
+++ schema/schema.js
@@ -0,0 +1,42 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const users = [
+	{
+		id: '23',
+		firstName: 'CARLOS',
+		age: 55,
+	},
+	{
+		id: '21',
+		firstName: 'ADA',
+		age: 5,
+	},
+];
+
+const UserType = new GraphQLObjectType({
+	name: 'User',
+	fields: {
+		id: { type: GraphQLString },
+		firstName: { type: GraphQLString },
+		age: { type: GraphQLInt },
+	},
+});
+
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		user: {
+			type: UserType,
+			args: { id: { type: GraphQLString } },
+			resolve (parentValue, args) {
+				return _.find(users, { id: args.id });
+			},
+		},
+	},
+});
+
+module.exports = new GraphQLSchema({
+	query: RootQuery,
+});
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
+const app = express();
+app.use(
+	'/graphql',
+	expressGraphQL({
+		schema,
+		graphiql: true,
+	})
+);
+
+const PORT = 4000 || process.env.PORT;
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
